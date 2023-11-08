608ca20dcf5cd39297c32481ab909ecf5cc0020d
matiasezequieldimuro <mdimuro@hexagonconsulting.net>
2023-11-08 14:28:48 -0300

    Fix deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/my-portfolio/"
-  }
-  return config
+export default defineConfig( {
+  plugins: [react()],
+  base: "/my-portfolio/"
 })
